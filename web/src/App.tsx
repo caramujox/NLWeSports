61c558981e606b054c1f54fe750b3c4af24c2da9
Caio de Araujo <caioa.morais@gmail.com>
2022-09-13 13:13:57 -0300

    Fim setup Web - Começando setup mobile

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,21 @@
 //JSX: Javascript+XML
 
+interface ButtonProps {
+  title: string
+}
+
+function Button(props: ButtonProps) {
+  return <button>{props.title}</button>
+}
 function App() {
-  return <h1>Hello NLW!</h1>
+  return (
+    <div>
+      <h1>Hello NLW!</h1>
+      <Button title="Send 1" />
+      <Button title="hello" />
+      <Button title="worlds!" />
+    </div>
+  )
 }
 
 export default App
