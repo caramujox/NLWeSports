b715915202c352dea79f7d97aba828bd4641f6f1
Caio de Araujo <caioa.morais@gmail.com>
2022-09-15 08:59:34 -0300

    Fim Web, começando Mobile

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 import './styles/main.css'
 import logoImg from './assets/Logo.svg'
+import { MagnifyingGlassPlus } from 'phosphor-react'
 
 function App() {
   return (
@@ -11,28 +12,76 @@ function App() {
         <span className="bg-nlw-gradient bg-clip-text text-transparent">
           duo
         </span>{' '}
-        está aqui
+        está aqui!
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <a href="">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game1.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="text-white font-bold block">
+              League of Legends
+            </strong>
+            <span className="text-sm text-zinc-300 block">4 anúncios</span>
+          </div>
         </a>
-        <a href="">
+        <a href="" className="relative rounded-l overflow-hidden">
           <img src="/game2.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="text-white font-bold block">Dota 2</strong>
+            <span className="text-sm text-zinc-300 block">4 anúncios</span>
+          </div>
         </a>
-        <a href="">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game3.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="text-white font-bold block">CS:GO</strong>
+            <span className="text-sm text-zinc-300 block">4 anúncios</span>
+          </div>
         </a>
-        <a href="">
+        <a href="" className="relative">
           <img src="/game4.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="text-white font-bold block">APEX</strong>
+            <span className="text-sm text-zinc-300 block">4 anúncios</span>
+          </div>
         </a>
-        <a href="">
+        <a href="" className="relative rounded-lg overflow-hidden">
           <img src="/game5.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="text-white font-bold block">Fortnite</strong>
+            <span className="text-sm text-zinc-300 block">4 anúncios</span>
+          </div>
         </a>
-        <a href="">
+        <a href="" className="relative rounded-l overflow-hidden">
           <img src="/game6.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="text-white font-bold block">
+              World of Warcraft
+            </strong>
+            <span className="text-sm text-zinc-300 block">4 anúncios</span>
+          </div>
         </a>
+      </div>
+
+      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8">
+        <div className="bg-[#2A2634] py-6 px-8 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black block">
+              Não encontrou seu duo?
+            </strong>
+            <span className="text-zinc-400 block">
+              Publique um anúncio para encontrar novos players!
+            </span>
+          </div>
+
+          <div>
+            <button className="py-3 px-4 bg-violet-500 text-white rounded-md hover:bg-[#43E7AD] flex items-center gap-3 transition-all duration-300 ease-in-out">
+              <MagnifyingGlassPlus size={24} />
+              Publicar Anúncio
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   )
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,11 +2,16 @@
 module.exports = {
   content: ['./src/**/*.tsx', './index.html'],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
     extend: {
       backgroundImage: {
         background: "url('/Fundo.png')",
         'nlw-gradient':
-          'linear-gradient(120.86deg, #9572FC 15.08%, #43E7AD 75.94%, #E1D55D 89.57%)'
+          'linear-gradient(120.86deg, #9572FC 15.08%, #43E7AD 75.94%, #E1D55D 89.57%)',
+        'game-gradient':
+          'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%);'
       }
     }
   },
