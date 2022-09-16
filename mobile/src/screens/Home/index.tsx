25fb11413b40dde84cf4f303074cb19ceb41ea99
Caio de Araujo <caioa.morais@gmail.com>
2022-09-16 18:48:28 -0300

    Fim aula 2

---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,28 @@
+import { View, Image, FlatList } from 'react-native'
+
+import { styles } from './styles'
+import logoImg from '../../assets/logo-nlw-esports.png'
+import { Heading } from '../../components/Heading'
+import { GameCard } from '../../components/GameCard'
+import { GAMES } from '../../utils/games'
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo}></Image>
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      ></Heading>
+
+      <FlatList
+        data={GAMES}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => <GameCard data={item}></GameCard>}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.contentList}
+      ></FlatList>
+    </View>
+  )
+}
